c7ad4e81fd908020797e2aebcf8e454a7fa2833c
Chad O'Brien <obrien.music@gmail.com>
2021-10-19 19:05:12 -0400

    first stab at create.php endpoint

--- api/create.php
+++ api/create.php
@@ -1,7 +1,7 @@
 <?php
 
     //connect to database
-    $con = mysqli_connect("localhost", "root", "", "API_DATA");
+    $con = mysqli_connect("localhost", "root", "", "PRACTICE_LOG");
     
     $response = array();
 
@@ -22,19 +22,35 @@
 
         //query database
 
-        $sql_email = "";
-        $result = mysqli_query($con, $sql_email);
+        $sql_email = "SELECT * FROM `users` WHERE 'email'='$email'";
+        $result_email = mysqli_query($con, $sql_email);
 
-        $sql_username = "";
-        $result = mysqli_query($con, $sql_username);
+        $sql_username = "SELECT * FROM `users` WHERE 'username'='$username'";
+        $result_username = mysqli_query($con, $sql_username);
 
-        $sql_add = "INSERT INTO `table name`(`name`, `email`, `username`, `password`) VALUES ('$name','$email','$username','$password')";
-        $result = mysqli_query($con, $sql_add);
         
         //send back response in response array
-            //if email already exists -> prompt forgot password page
-            //else, if username exists -> prompt for new username
-            //else, add new user to database -> success message
+
+            if (mysqli_num_rows($result_email) != 0) {    //if email already exists -> prompt forgot password page
+
+                $response["message"] = "Email is already registered";
+
+            } else if (mysqli_num_rows($result_username) != 0) {  //else, if username exists -> prompt for new username
+
+                $response["message"] = "Username is taken";
+
+            } else {    //else, add new user to database -> success message
+                
+                $sql_add = "INSERT INTO `table name`(`name`, `email`, `username`, `password`) VALUES ('$name','$email','$username','$password')";
+                $result = mysqli_query($con, $sql_add);
+                
+                if ($result) {
+                    $response["message"] = "User succesfully created";
+                } else {
+                    $response["message"] = "User creation failed";
+                }
+            }
+
 
         echo json_encode($response);
         mysqli_close($con);
