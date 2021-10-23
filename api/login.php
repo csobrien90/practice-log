31f9396d13bb3e92d2f30b7e04e5df5bcc523297
Chad O'Brien <obrien.music@gmail.com>
2021-10-23 13:42:11 -0400

    add basic login functionality

--- api/login.php
+++ api/login.php
@@ -1,7 +1,7 @@
 <?php
 
     //connect to database
-    $con = mysqli_connect("localhost", "root", "", "API_DATA");
+    $con = mysqli_connect("localhost", "root", "", "practice_log");
 
     $response = array();
 
@@ -20,17 +20,29 @@
 
         //query database
 
-        $sql_username = "";
+        $sql_username = "SELECT * FROM `users` WHERE username='$username'";
         $result_username = mysqli_query($con, $sql_username);
 
-        $sql_password = "";
+        $sql_password = "SELECT * FROM `users` WHERE username='$username' AND password='$password'";
         $result_password = mysqli_query($con, $sql_password);
 
         //send back response in $response array:
-            //if username does not exist
-            //if username exists but password does not match
-            //if username exists and password matches -> acceptance code and real name of user
-        
+
+        if (mysqli_num_rows($result_username) == 0) {    //if username does not exist prompt forgot password page
+
+                $response["message"] = "Username not found";
+
+            } else if (mysqli_num_rows($result_password) == 0) {    //if username exists but password does not match
+
+                $response["message"] = "Incorrect password";
+
+            } else {    //if username exists and password matches
+                
+                $response["message"] = "Login successful";
+                //get full user data and return in $response
+                
+            }
+
         echo json_encode($response);
         mysqli_close($con);
 
