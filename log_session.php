6bd8f810a441a55a972d594d9d2ce58eed2c0810
Chad O'Brien <obrien.music@gmail.com>
2021-10-23 22:23:08 -0400

    finish site structure

---
+++ log_session.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>New Session | Practice Log</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <header>
+        <?php include 'header.php' ?>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        <?php include 'footer.php' ?>
+    </footer>
+
+</body>
+
+<script>
+    
+</script>
+
+</html>
